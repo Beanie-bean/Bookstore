40771412f4d66bd4cc61ca8628b1f8b1e566c469
Veera Virtanen <veera@Veeras-MacBook-Pro.local>
2024-09-24 15:52:39 +0300

    Spring REST lisätty

--- src/main/java/hh/sof03/bookstore/domain/Book.java
+++ src/main/java/hh/sof03/bookstore/domain/Book.java
@@ -1,5 +1,7 @@
 package hh.sof03.bookstore.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -20,6 +22,7 @@ public class Book {
     private Integer price;
 
     @ManyToOne
+    @JsonIgnoreProperties("books")
     @JoinColumn(name = "categoryId")
     private Category category;
 
@@ -44,70 +47,70 @@ public class Book {
         this.category = category;
     }
 
-    
     public String getTitle() {
         return title;
     }
-    
+
     public void setTitle(String title) {
         this.title = title;
     }
-    
+
     public String getAuthor() {
         return author;
     }
-    
+
     public void setAuthor(String author) {
         this.author = author;
     }
-    
+
     public Integer getPublicationYear() {
         return publicationYear;
     }
-    
+
     public void setPublicationYear(Integer publicationYear) {
         this.publicationYear = publicationYear;
     }
-    
+
     public String getIsbn() {
         return isbn;
     }
-    
+
     public void setIsbn(String isbn) {
         this.isbn = isbn;
     }
-    
+
     public Integer getPrice() {
         return price;
     }
-    
+
     public void setPrice(Integer price) {
         this.price = price;
     }
-    
+
     public Long getId() {
         return id;
     }
-    
+
     public void setId(Long id) {
         this.id = id;
     }
-    
+
     public Category getCategory() {
         return category;
     }
 
-    public void  setCategory(Category category) {
+    public void setCategory(Category category) {
         this.category = category;
     }
 
-    
     @Override
     public String toString() {
         if (this.category != null)
-            return "Book [title=" + title + ", author=" + author + ", publicationYear=" + publicationYear + ", isbn=" + isbn + ", price=" + price + " category =" + this.getCategory() + "]";
-        else 
-            return "Book [title=" + title + ", author=" + author + ", publicationYear=" + publicationYear + ", isbn=" + isbn + ", price=" + price + "]";
+            return "Book [title=" + title + ", author=" + author + ", publicationYear=" + publicationYear + ", isbn="
+                    + isbn + ", price=" + price + " category =" + this.getCategory() + "]";
+        else
+            return "Book [title=" + title + ", author=" + author + ", publicationYear=" + publicationYear + ", isbn="
+                    + isbn + ", price=" + price + "]";
     }
 
 }
