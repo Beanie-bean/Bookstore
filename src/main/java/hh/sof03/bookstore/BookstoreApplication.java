ebcedf30c99d1f169f7c7446e11db3071d1bc641
Veera Virtanen <veera@Veeras-MacBook-Pro.local>
2024-10-18 14:49:26 +0300

    Dockerfile muokattu

--- src/main/java/hh/sof03/bookstore/BookstoreApplication.java
+++ src/main/java/hh/sof03/bookstore/BookstoreApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import hh.sof03.bookstore.domain.AppUser;
 import hh.sof03.bookstore.domain.AppUserRepository;
